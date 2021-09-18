2b35261a0e628a679d18aaf47747924ec821ecd4
Yaroslav <ray.last.light@gmail.com>
2021-09-18 16:09:17 +0300

    Add views files

---
+++ application/views/account/login.php
@@ -0,0 +1,8 @@
+<h3>Login</h3>
+<form action="/account/login" method="post">
+    <p>Login: </p>
+    <p><input type="text" name="login" id=""></p>
+    <p>Password:</p>
+    <p><input type="text" name="password" id=""></p>
+    <b><button type="submit" name="enter">Login</button></b>
+</form>
---
+++ application/views/account/register.php
@@ -0,0 +1,8 @@
+<h3>Registration</h3>
+<form action="">
+    <p>Login: </p>
+    <p><input type="text" name="" id=""></p>
+    <p>Password:</p>
+    <p><input type="password" name="" id=""></p>
+    <b><button>Registration</button></b>
+</form>
