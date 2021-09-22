056a80c7e3e2b5c8c6c11caee698207044e0613c
Yaroslav <ray.last.light@gmail.com>
2021-09-22 19:31:43 +0300

    Update View.php
    Add static function for return code errors.

--- application/core/View.php
+++ application/core/View.php
@@ -29,3 +29,14 @@
             }
 
         }
+
+        public static function errorCode($code)
+        {
+            http_response_code($code);
+            $path = require 'application/views/errors/' . $code . '.php';
+            if (file_exists($path)) 
+            {
+                require $path;
+            }
+            exit;
+        }
