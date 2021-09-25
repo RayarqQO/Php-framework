b3636fc5b2d401c35b27dc1c57c457fe41d83b95
Yaroslav <ray.last.light@gmail.com>
2021-09-25 18:49:00 +0300

    Create AccountController.php
    Account controller with 2 actions.

---
+++ application/controllers/AccountController.php
@@ -0,0 +1,23 @@
+<?php
+
+    namespace application\controllers;
+
+    use application\core\Controller;
+
+    class AccountController extends Controller
+    {
+        public function loginAction()
+        {
+            if (!empty($_POST)) 
+            {
+                $this->view->message('status', 'message');
+            }
+            $this->view->render('Вход');
+        }
+
+        public function registerAction()
+        {
+            $this->view->render('Регистрация');
+        }
+    }
+    
