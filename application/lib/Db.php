c17f006a688444a684178649dae0ba01fdbe0ba8
Yaroslav <ray.last.light@gmail.com>
2021-09-25 16:54:43 +0300

    Update Db.php
    Returns a single column from the next row of a result set.

--- application/lib/Db.php
+++ application/lib/Db.php
@@ -37,3 +37,9 @@ class Db
         return $result->fetchAll(PDO::FETCH_ASSOC);
     }
 
+    public function column($sql, $params = [])
+    {
+        $result = $this->query($sql, $params);
+        return $result->fetchColumn();
+    }
+}
