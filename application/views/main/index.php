bcfa36039161141e317532d98e41c8a63dac0eca
Yaroslav <ray.last.light@gmail.com>
2021-09-25 17:41:08 +0300

    Update index.php

--- application/views/main/index.php
+++ application/views/main/index.php
@@ -1,6 +1,6 @@
 <p>Галавная страница</p>
 
-<?php foreach($vars['news'] as $val): ?>
+<?php foreach($news as $val): ?>
     <h3><?php echo $val['title']; ?></h3>
     <p><?php echo $val['description']; ?></p>
 <?php endforeach; ?>
