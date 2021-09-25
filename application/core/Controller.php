e862ffa05f44bc0c030f5cd857d2398677ff5619
Yaroslav <ray.last.light@gmail.com>
2021-09-25 17:10:09 +0300

    Create Controller.php

---
+++ application/core/Controller.php
@@ -0,0 +1,18 @@
+<?php
+
+    namespace application\core;
+
+    use application\core\View;
+
+    abstract class Controller
+    {
+        public $route;
+        public $view;
+        public $acl;
+
+        public function __construct($route)
+        {
+            $this->route = $route;
+            $this->view = new View($route);
+            $this->model = $this->loadModel($route['controller']);
+        }
